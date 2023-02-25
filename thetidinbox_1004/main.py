32d3c683525d8908ceac97ebf744477789922039
lyna el ayoubi <lyna.elayoubi@gmail.com>
2023-02-25 14:40:21 +0100

    updates urgent

--- thetidinbox_1004/main.py
+++ thetidinbox_1004/main.py
@@ -5,6 +5,7 @@ from model_spam import count_vectorizer, term_frequency,tfidf, model_spam, save_
 from model_pro_perso import vectorizer, model_pro_perso, save_pipeline_pro_perso,load_pipeline_pro_perso
 from model_action_word import action_dict, is_word_present, which_word_present, which_column_present, unique_values
 from model_meeting_invit import classify_meeting_invit
+from model_urgent_word import urgent_vocab_dict
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import mean_squared_error
 from math import sqrt
@@ -132,6 +133,17 @@ def pred_pro_perso(X_pred: pd.DataFrame = None) -> np.ndarray:
 
     return y_pred
 
+def urgent_categories(df, urgent_df=urgent_vocab_dict()):
+    df['body'] = df['body'].astype(str)
+    df["present"] = df.body.apply(lambda x: is_word_present(urgent_df, x))
+    df["wordspresent"] = df.body.apply(lambda x: which_word_present(urgent_df, x))
+    df["columnspresent"] = df.body.apply(lambda x: which_column_present(urgent_df, x))
+    #if not df["present"].any():
+        #return 'non urgent'
+    #if df["present"].any() == False:
+        #return 'non urgent'
+    return df
+
 def action_categories(df, action_df=action_dict()):
     df['body'] = df['body'].astype(str)
     df["present"] = df.body.apply(lambda x: is_word_present(action_df, x))
@@ -153,14 +165,17 @@ if __name__ == '__main__':
     # Prediction
     spam = pred_spam(pd.DataFrame({"message": [test]}))
     pro_perso = pred_pro_perso(pd.DataFrame({"body": [test]}))
-    action_class = action_categories(pd.DataFrame({"body": [test]}))
-    unique_action_class = unique_values(action_class['columnspresent'])
-    meeting = pred_meeting_invit(pd.Series(test))
+    urgent_class = urgent_categories(pd.DataFrame({"body": [test]}))['present']
+    #unique_urgent_class = unique_values(urgent_class['columnspresent'])
+#action_class = action_categories(pd.DataFrame({"body": [test]}))
+    #unique_action_class = unique_values(action_class['columnspresent'])
+    #meeting = pred_meeting_invit(pd.Series(test))
 
     # Return
-    d_spam = {0:"==>Not a spam", 1:"==>Spam"}
-    d_pro_perso = {0:"==>Professional", 1:"==>Personal"}
-    d_meeting_invit = {0:"==>Not a meeting invit", 1:"==>Meeting invitation"}
+    #d_spam = {0:"==>Not a spam", 1:"==>Spam"}
+    #d_pro_perso = {0:"==>Professional", 1:"==>Personal"}
+    #d_meeting_invit = {0:"==>Not a meeting invit", 1:"==>Meeting invitation"}
 
 
-    print (d_spam[spam[0]], d_pro_perso[pro_perso[0]], unique_action_class, d_meeting_invit[meeting[0]])
+    #print (d_spam[spam[0]], d_pro_perso[pro_perso[0]],unique_urgent_class, unique_action_class, d_meeting_invit[meeting[0]])
+    print(urgent_class)
